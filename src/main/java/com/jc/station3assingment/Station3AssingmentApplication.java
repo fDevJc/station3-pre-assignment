9b58aa4960ed50c731aab092b65a146100ca91cb
fDevJc <fdevjc@gmail.com>
2023-03-14 13:52:36 +0900

    #1 init commit

---
+++ src/main/java/com/jc/station3assingment/Station3AssingmentApplication.java
@@ -0,0 +1,13 @@
+package com.jc.station3assingment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Station3AssingmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Station3AssingmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/station3assingment/Station3AssingmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.station3assingment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station3AssingmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
