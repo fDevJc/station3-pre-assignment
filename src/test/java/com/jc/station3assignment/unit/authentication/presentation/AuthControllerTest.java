fd1bfb747e1ff5a7f66db36086fa3002de4e289a
fDevJc <fdevjc@gmail.com>
2023-03-15 20:21:58 +0900

    test: restdocs 적용(#17)

--- src/test/java/com/jc/station3assignment/unit/authentication/presentation/AuthControllerTest.java
+++ src/test/java/com/jc/station3assignment/unit/authentication/presentation/AuthControllerTest.java
@@ -1,16 +1,20 @@
 package com.jc.station3assignment.unit.authentication.presentation;
 
+import static com.jc.station3assignment.config.docs.RestDocsUtils.*;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -21,6 +25,7 @@ import com.jc.station3assignment.authentication.application.dto.response.SignupR
 import com.jc.station3assignment.authentication.presentation.AuthController;
 import com.jc.station3assignment.authentication.presentation.dto.request.SignupRequest;
 
+@AutoConfigureRestDocs
 @WebMvcTest({AuthController.class})
 public class AuthControllerTest {
 	@Autowired
@@ -42,7 +47,12 @@ public class AuthControllerTest {
 			.nickname("nickname")
 			.build();
 
-		SignupResponseDto serviceResult = SignupResponseDto.builder().id(1L).build();
+		SignupResponseDto serviceResult = SignupResponseDto.builder()
+			.id(1L)
+			.email("fdevjc@gmail.com")
+			.nickname("nickname")
+			.build();
+
 		given(authService.signup(any(SignupRequestDto.class)))
 			.willReturn(serviceResult);
 
@@ -58,6 +68,22 @@ public class AuthControllerTest {
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 			.andExpect(content().string(objectMapper.writeValueAsString(serviceResult)));
 
-		resultActions.andDo(print());
+		//doc
+		resultActions
+			.andDo(document("post-signup",
+				getRestDocRequest(),
+				getRestDocResponse(),
+				requestFields(
+					fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+					fieldWithPath("password").type(JsonFieldType.STRING).description("패스워드"),
+					fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+					fieldWithPath("name").type(JsonFieldType.STRING).optional().description("이름"),
+					fieldWithPath("phoneNumber").type(JsonFieldType.STRING).optional().description("휴대폰")
+				),
+				responseFields(
+					fieldWithPath("id").type(JsonFieldType.NUMBER).description("사용자 아이디(내부)"),
+					fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+					fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임")
+				)));
 	}
 }
