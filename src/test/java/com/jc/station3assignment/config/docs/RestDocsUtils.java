d220dd8d03681afbee58b78ffb69482bbfbc59d6
fDevJc <fdevjc@gmail.com>
2023-03-15 20:16:38 +0900

    test: Document pretty 설정 (#13)

---
+++ src/test/java/com/jc/station3assignment/config/docs/RestDocsUtils.java
@@ -0,0 +1,17 @@
+package com.jc.station3assignment.config.docs;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+public interface RestDocsUtils {
+	static OperationRequestPreprocessor getRestDocRequest(){
+		return preprocessRequest(prettyPrint());
+	}
+
+	static OperationResponsePreprocessor getRestDocResponse(){
+		return preprocessResponse(prettyPrint());
+	}
+
+}
