a6f2a78d9b13d289887ed12079b25183464e72ff
fDevJc <fdevjc@gmail.com>
2023-03-18 16:16:25 +0900

    test: 검색 파라미터 위한 애노테이션 관련 테스트 (#7)

---
+++ src/test/java/com/jc/station3assignment/config/search/extractor/DefaultQueryExtractorTest.java
@@ -0,0 +1,24 @@
+package com.jc.station3assignment.config.search.extractor;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.jc.station3assignment.config.search.QueryExtractor;
+
+class DefaultQueryExtractorTest {
+	private QueryExtractor queryExtractor = new DefaultQueryExtractor();
+
+	@Test
+	void 쿼리스트링에서_쿼리를_추출한다() throws Exception {
+		//given
+		String queryString = "q=\"roomType\":[\"ONE_ROOM\"],\"dealType\":[\"LONG_TERM_RENT\"],\"depositRange\": [0,10000],\"rentRange\": [0,100]&other=123";
+
+		//when
+		String ret = queryExtractor.extract(queryString);
+
+		//then
+		System.out.println("ret = " + ret);
+		Assertions.assertThat(ret).isEqualTo("\"roomType\":[\"ONE_ROOM\"],\"dealType\":[\"LONG_TERM_RENT\"],\"depositRange\": [0,10000],\"rentRange\": [0,100]");
+
+	}
+}
