dbdb63216ab84f43b5e53752225645a14d8142c3
fDevJc <fdevjc@gmail.com>
2023-03-18 21:16:53 +0900

    refactor: 중복코드 메소드 추출 (#34)

--- src/main/java/com/jc/station3assignment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jc/station3assignment/exception/GlobalExceptionHandler.java
@@ -21,44 +21,47 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(PermissionDeniedRoomException.class)
 	public ResponseEntity<ErrorResponse> permissionDeniedRoomException(PermissionDeniedRoomException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(RoomNotFoundException.class)
 	public ResponseEntity<ErrorResponse> roomNotFoundException(RoomNotFoundException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<ErrorResponse> userNotFoundException(UserNotFoundException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(InvalidTokenException.class)
 	public ResponseEntity<ErrorResponse> invalidTokenException(InvalidTokenException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(AuthenticationUserException.class)
 	public ResponseEntity<ErrorResponse> authenticationUserException(AuthenticationUserException e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> exception(Exception e) {
-		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+		logWarn(e);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
 	}
 
+	private static void logWarn(Exception e) {
+		log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
+	}
 }
